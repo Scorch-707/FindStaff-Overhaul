8da5c52ceaa205f7d4027ed14de3eb77df991a5c
traynerZero <znotsukaima@gmail.com>
2018-01-18 00:07:10 +0800

    clean dashboard

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 0.0.1
+    </div>
+    <strong>Copyright &copy; 2018 <a href="#">Find Staff</a>.</strong> All rights
+    reserved.
+  </footer>
---
+++ sidebar.php
@@ -0,0 +1,89 @@
+<!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Leia Rose Maza</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        
+        <li class="active treeview">
+          <a href="index.html"><span>Dashboard</a></span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+        </li>
+        <li class="active treeview">
+          <a href="#">
+            <span><strong>Maintenance</strong></span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 1</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 2</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 3</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 4</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 5</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 6</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 7</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 8</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 9</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Maintenance 10</a></li>
+          </ul>
+        </li>
+        <li class="active treeview">
+          <a href="#">
+            <span><strong>Transactions</strong></span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i>Transactions 1</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Transactions 2</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Transactions 3</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Transactions 4</a></li>
+          </ul>
+        </li>
+         <li class="active treeview">
+          <a href="#">
+            <span><strong>Utilities</strong></span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i>Utilities 1</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Utilities 2</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Utilities 3</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Utilities 4</a></li>
+          </ul>
+        </li>
+         <li class="active treeview">
+          <a href="#">
+            <span><strong>Reports</strong></span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i>Reports 1</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Reports 2</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Reports 3</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Reports 4</a></li>
+          </ul>
+        </li>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
