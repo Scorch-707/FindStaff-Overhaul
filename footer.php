1dbf9e4dcc6b94867318f46c0ca39a03866ebdad
Leia Rose <donutdouche@gmail.com>
2018-01-18 23:00:24 +0800

    try lang

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 0.0.1
     </div>
-    <strong>Copyright &copy; 2018 <a href="#">Find Staff</a>.</strong> All rights
+    <strong>Copyright &copy; 2018 <a href="#">FindStaff Placement Services, Inc.</a>.</strong> All rights
     reserved.
   </footer>
